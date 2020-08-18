e7daa76b2dd7140f2586cf91f0c03aa2a5b0c19c
Tom Cranitch <tom@cranitch.com.au>
2020-08-18 20:43:32 +1000

    Add comments

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,14 @@ import { User } from "./models/User";
 async function main() {
 	const connection = await createConnection();
 
+	// Sample db insert command
 	console.log(await User.create({
 		id: 123,
 		name: 'User',
 		email: 'aaa@bbb.ccc'
 	}).save());
 
+	// Sample db query command
 	console.log(await User.find());
 }
 
