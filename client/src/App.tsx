67af18690e77d1b0c6933e5c316961230905a426
Tom Cranitch <tom@cranitch.com.au>
2020-08-22 12:35:23 +1000

    Fixes for pull request
     - Move sidebar to component
     - Add github action

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Divider from "@material-ui/core/Divider";
-import Drawer from "@material-ui/core/Drawer";
-import Hidden from "@material-ui/core/Hidden";
 import IconButton from "@material-ui/core/IconButton";
-import HomeIcon from "@material-ui/icons/Home";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
 import MenuIcon from "@material-ui/icons/Menu";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
@@ -23,6 +15,7 @@ import {
 } from "@material-ui/core/styles";
 
 import modules from "./modules";
+import Sidebar from "./components/Sidebar";
 
 const drawerWidth = 240;
 
@@ -30,12 +23,6 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: "flex",
-    },
-    drawer: {
-      [theme.breakpoints.up("sm")]: {
-        width: drawerWidth,
-        flexShrink: 0,
-      },
     },
     appBar: {
       [theme.breakpoints.up("sm")]: {
@@ -79,42 +66,22 @@ export default function App() {
       ].join(","),
     },
   });
+
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
-  const drawer = (
-    <div>
-      <div className={classes.toolbar} />
-      <Divider />
-      <List>
-        <ListItem button key="Home" {...{ component: NavLink, to: "/" }}>
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItem>
-        {modules.map((item) => (
-          <ListItem
-            button
-            key={item.name}
-            {...{ component: NavLink, to: item.routeProps.path }}
-          >
-            <ListItemIcon>{<item.icon />}</ListItemIcon>
-            <ListItemText primary={item.name} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <div className={classes.root}>
           <CssBaseline />
+          <Sidebar
+            mobileOpen={mobileOpen}
+            handleDrawerToggle={handleDrawerToggle}
+          />
           <AppBar position="fixed" className={classes.appBar}>
             <Toolbar>
               <IconButton
@@ -131,36 +98,6 @@ export default function App() {
               </Typography>
             </Toolbar>
           </AppBar>
-          <nav className={classes.drawer} aria-label="mailbox folders">
-            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-            <Hidden smUp implementation="css">
-              <Drawer
-                variant="temporary"
-                anchor={theme.direction === "rtl" ? "right" : "left"}
-                open={mobileOpen}
-                onClose={handleDrawerToggle}
-                classes={{
-                  paper: classes.drawerPaper,
-                }}
-                ModalProps={{
-                  keepMounted: true, // Better open performance on mobile.
-                }}
-              >
-                {drawer}
-              </Drawer>
-            </Hidden>
-            <Hidden xsDown implementation="css">
-              <Drawer
-                classes={{
-                  paper: classes.drawerPaper,
-                }}
-                variant="permanent"
-                open
-              >
-                {drawer}
-              </Drawer>
-            </Hidden>
-          </nav>
           <main className={classes.content}>
             <div className={classes.toolbar} />
             {modules.map((module) => (
