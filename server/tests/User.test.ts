31db95cea92f65b2841af72de7d78221c0824a16
Tom Cranitch <tom@cranitch.com.au>
2020-08-22 20:00:52 +1000

    Given up on testing

--- server/tests/User.test.ts
+++ server/tests/User.test.ts
@@ -3,12 +3,18 @@ import {createTestConnection} from "./TestDatabase";
 import {graphqlTestCall} from "./GraphqlTestCall";
 import {useSeeding, runSeeder} from "@doofenshmirtz-deco-inc/typeorm-seeding";
 import UserSeeder from "../src/db/seeds/UserSeeder";
+import {isAuthMock} from "./isAuthMock";
+import {ResolverData, NextFn} from "type-graphql";
+import {Context} from "../src/middleware/Context";
 
+let uid = "test-uid";
+
+/* TODO this is currently broken
 jest.mock("../src/middleware/isAuth", () => ({
 	__esModule: true,
-	isAuth: jest.fn(() => console.log("isAuth isn't implemented"))
+	isAuth: jest.fn((data: ResolverData<Context>, next: NextFn) => isAuthMock(data, next, uid)),
 }));
-
+*/
 
 const getUserQuery = `
 	query {
@@ -21,6 +27,15 @@ const getUserQuery = `
 const getUsersQuery = `
 	query {
 		users {
+			name
+		}
+	}
+`;
+
+
+const meQuery = `
+	query {
+		me {
 			name
 		}
 	}
@@ -40,11 +55,6 @@ afterAll(async () => {
 	await connection.close();
 });
 
-const setup = (overrides: any) => {
-	
-};
-
-
 describe("User Resolver", () => {
 	it("user query", async () => {
 		const user = await graphqlTestCall(getUserQuery);
@@ -54,7 +64,19 @@ describe("User Resolver", () => {
 	});
 
 	it("users query", async () => {
+		// TODO test properly when there is actual 
+		uid = "test-uid";	
+
 		const users = await graphqlTestCall(getUsersQuery);
+	});
+
+
+	it("users query", async () => {
+		uid = "doof-uid";	
+
+		const me = await graphqlTestCall(meQuery);
+		console.log(me);
+		expect(me.data?.name).toBe('Heinz Doofenshmitz');
 	});
 });
 
--- server/tests/isAuthMock.ts
+++ server/tests/isAuthMock.ts
@@ -1,7 +1,10 @@
 import {ResolverData, NextFn, MiddlewareFn} from "type-graphql";
 import {Context} from "../src/middleware/Context";
 
+
+// For some reason data is undefined. I've given up for the moment
 export const isAuthMock = async (data: ResolverData<Context>, next: NextFn, uid: string) => {
+	console.log(data);
 	data.context.payload = { uid };
 
 	return next();
