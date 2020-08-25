ce4469a9659337a587ee4a858811fe4815683833
Zenzlia <natalie.hong@uqconnect.edu.au>
2020-08-26 09:20:03 +1000

    oops

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -73,32 +73,6 @@ export default function App() {
     setMobileOpen(!mobileOpen);
   };
 
-<<<<<<< HEAD
-import { Button } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          <Button primary onClick={() => alert('Clicked!')}>Semantic UI Button</Button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
   return (
     <Router>
       <ThemeProvider theme={theme}>
@@ -133,6 +107,5 @@ function App() {
         </div>
       </ThemeProvider>
     </Router>
->>>>>>> matt
   );
 }
