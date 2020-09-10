b06300b33398a84d16571f1e427e6f6b6926b11c
Zenzlia <natalie.hong@uqconnect.edu.au>
2020-09-10 17:45:07 +1000

    Added explore and recent contacts page, styling hurts me

--- client/src/modules/StudyRooms/explore.tsx
+++ client/src/modules/StudyRooms/explore.tsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles, Theme } from '@material-ui/core/styles';
+import List from "@material-ui/core/List";
+import TextField from "@material-ui/core/TextField";
+import openRooms from '../../components/openRooms.json'
+import StudyRoom from "../../components/StudyRoom";
 
 const Explore = () => {
-    const useStyles = makeStyles((theme: Theme) => ({
-      root: {
-        flexGrow: 3,
-        backgroundColor: theme.palette.background.paper,
-        display: "flex"
-      }
-    }));
-    const classes = useStyles();
+  const [searchTerm, setSearchTerm] = useState("");
+  const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+      backgroundColor: theme.palette.background.paper,
+      display: "flex",
+      flexDirection: "column",
+    },
+    searchBar : {
+      width: "25%",
+      fontSize: "1.75rem"
+    }
+  }));
+  const classes = useStyles();
 
-    return (
-      <div className={classes.root}>
-        Explore I am lazy
-      </div>
-    );
+  return (
+    <List className={classes.root}>
+      <TextField
+        id="contacts-search"
+        label="Search For Open Meeting Rooms"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className={classes.searchBar}
+      />
+      {Object.values(openRooms).map((item) =>
+        item.title.toLowerCase().includes(searchTerm.toLowerCase()) ? (
+          <StudyRoom room={item}/>
+        ) : null
+      )}
+    </List>
+  );
 };
 
 export default Explore;
