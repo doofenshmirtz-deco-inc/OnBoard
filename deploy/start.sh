32188e3838a209710a9150b6b220a1a9b8082ef3
Kenton Lam <kenton_lam@outlook.com>
2020-09-23 16:58:09 +1000

    Decrypt from Systems Manager key.

--- deploy/start.sh
+++ deploy/start.sh
@@ -6,8 +6,12 @@ cd "$(dirname "$0")"/..
 
 docker network create meet.jitsi || true
 
+aws ssm get-parameters --region ap-southeast-2 --names git-crypt --with-decryption --query Parameters[0].Value > .key
+git-crypt unlock .key
+
 cd server
 cat ./TOKEN.txt | docker login https://docker.pkg.github.com -u kentonlam --password-stdin
+yarn
 yarn run docker:up
 cd -
 
