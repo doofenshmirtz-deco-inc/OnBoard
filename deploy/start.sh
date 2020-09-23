4419484ad58d134c9cdd32040480073fdb86ccb2
Kenton Lam <kenton_lam@outlook.com>
2020-09-23 17:32:03 +1000

    Decrypt properly?

--- deploy/start.sh
+++ deploy/start.sh
@@ -6,8 +6,12 @@ cd "$(dirname "$0")"/..
 
 docker network create meet.jitsi || true
 
-aws ssm get-parameters --region ap-southeast-2 --names git-crypt --with-decryption --query Parameters[0].Value > .key
-git-crypt unlock .key
+aws ssm get-parameters --region ap-southeast-2 --names git-crypt --with-decryption --query Parameters[0].Value \
+    | tr -d \" | base64 -d > .key
+for f in server/TOKEN.txt server/.npmrc; do
+    git-crypt smudge --key-file .key < $f > $f.out
+    mv $f.out $f
+done
 
 cd server
 cat ./TOKEN.txt | docker login https://docker.pkg.github.com -u kentonlam --password-stdin
