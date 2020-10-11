3d001d76fefe6b2cbaafac3d3d1987d55498264d
mattchrlw <mattchrlw@gmail.com>
2020-10-11 16:21:08 +1000

    Sorta jank

--- client/src/modules/Login/index.tsx
+++ client/src/modules/Login/index.tsx
@@ -4,6 +4,7 @@ import { gql, useApolloClient } from "@apollo/client";
 import * as firebase from "firebase";
 import { LoadingPage } from "../../components/LoadingPage";
 import { GetCustomToken } from "../../graphql/GetCustomToken";
+import Alert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -32,6 +33,10 @@ export const Login = () => {
   const [uid, setUID] = React.useState("");
   const [pass, setPass] = React.useState("");
   const [loading, setLoading] = React.useState(false);
+  const [incorrectPwd, setIncorrectPwd] = React.useState(false);
+  const [errorPwd, setErrorPwd] = React.useState(false);
+  const [errorCode, setErrorCode] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
 
   const submit = async () => {
     setLoading(true);
@@ -43,23 +48,51 @@ export const Login = () => {
       .result();
 
     if (!data || !data.getCustomToken.token) return; // TODO actual error handling pls
-    await firebase.auth().signInWithCustomToken(data.getCustomToken.token);
+    
+    await firebase.auth().signInWithEmailAndPassword(
+      uid + '@doofenshmirtz.xyz', pass
+    ).then(function(user) {
+      firebase.auth().signInWithCustomToken(data.getCustomToken.token);
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // It's a security thing to not differentiate between missing user and wrong password right?
+      if (errorCode === "auth/wrong-password" || errorCode === "auth/user-not-found") {
+        console.log(errorCode, errorMessage);
+        setIncorrectPwd(true);
+        setLoading(false);
+      // Any other sort of error
+      } else {
+        console.log(errorCode, errorMessage);
+        setErrorPwd(true);
+        setErrorCode(errorCode);
+        setErrorMessage(errorMessage);
+        setLoading(false);
+      }
+    });
   };
 
   if (loading) return <LoadingPage />;
 
   return (
     <Grid container direction="column" justify="center" alignItems="center">
-      <h1>Login</h1>
+      <h1>Welcome to OnBoard</h1>
       <form className={classes.root} onSubmit={submit}>
+        {incorrectPwd && <Alert severity="error">
+          Incorrect username or password. Please try again.
+        </Alert>}
+        {errorPwd && <Alert severity="error">
+          {errorCode}: {errorMessage}
+        </Alert>}
         <TextField
           id="standard-basic"
-          label="uid"
+          label="Username/Email"
           onChange={(e) => setUID(e.currentTarget.value)}
         />
         <TextField
           id="standard-input-password"
-          label="password"
+          label="Password"
           type="password"
           onChange={(e) => setPass(e.currentTarget.value)}
         />
@@ -70,3 +103,4 @@ export const Login = () => {
     </Grid>
   );
 };
+
