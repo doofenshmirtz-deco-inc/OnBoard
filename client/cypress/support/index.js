2acef6fd6d6ef103a744136495e6fbdb1f61dd9f
Tom Cranitch <tom@cranitch.com.au>
2020-10-14 08:59:42 +1000

    Fixes

--- client/cypress/support/index.js
+++ client/cypress/support/index.js
@@ -2,5 +2,6 @@ beforeEach(() => {
   indexedDB.deleteDatabase("firebaseLocalStorageDb");
   cy.visit("/");
   cy.get("[data-cy=username-field]").type("doof");
+  cy.get("[data-cy=password-field]").type("doofenshmirtz123");
   cy.get("[data-cy=login-button]").click();
 });
