98920ec791a17d7c60e4f95f5ee5b50f0f15c231
mattchrlw <mattchrlw@gmail.com>
2020-10-19 22:01:47 +1000

    Fixed tests

--- client/cypress/integration/Sidebar.spec.ts
+++ client/cypress/integration/Sidebar.spec.ts
@@ -26,7 +26,9 @@ describe("Sidebar Test", () => {
       .get(".MuiListItemIcon-root")
       .last()
       .click();
-    cy.get(".MuiButton-root").contains("OK").click();
+    cy.get(".MuiButton-root")
+      .contains("OK")
+      .click();
     cy.get("h1").contains("Welcome");
   });
 });
