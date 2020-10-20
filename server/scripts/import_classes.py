4351b944fe481bf4835baf7aea3820f110a3a99b
mattchrlw <mattchrlw@gmail.com>
2020-10-21 09:16:47 +1000

    Script in progress

--- server/scripts/import_classes.py
+++ server/scripts/import_classes.py
@@ -1,23 +1,70 @@
 from pytimeparse.timeparse import timeparse
+import csv
 
-classes_file = open("classes/Matthew_Low_matt.txt")
+# Deal with stupid double tabs in Allocate+ output
+with open("classes/Matthew_Low_matt.txt", "r+") as file:
+    content = file.read()
+    file.seek(0)
+    file.truncate()
+    file.write(content.replace('\t\t', '\t'))
 
-class_objects = []
+with open("classes/Matthew_Low_matt.txt", "r") as csvfile:
+    # Deal with extra tab
+    courses = []
+    classes = []
 
-print(classes_file.readline())
-classes = [a.split("\t") for a in classes_file.readlines()]
-for cl in classes:
-    class_objects.append({
-        'code': cl[0],
-        'description': cl[1],
-        'group': cl[2],
-        'activity': cl[3],
-        'day': cl[4],
-        'time': cl[5],
-        # Output duration in minutes
-        'duration': timeparse(cl[10]) // 60
-    })
+    reader = csv.DictReader(csvfile, delimiter="\t")
+    for line in reader:
+        code, sem, _, _ = line['Subject Code'].split("_")
+        courses.append({
+            'code': code,
+            'name': line['Description'],
+            'semester': sem,
+            'level': "CourseLevel.Undergrad" if int(code[4]) < 7 else "CourseLevel.Postgrad",
+            'year': 2020
+        })
 
-print(class_objects)
+        if "LEC" in line['Group']:
+            course_type = "ClassType.Lecture"
+        elif "TUT" in line['Group']:
+            course_type = "ClassType.Tutorial"
+        elif "PRA" in line['Group']:
+            course_type = "ClassType.Practical"
+        else:
+            course_type = "ClassType.Lecture"
 
-classes_file.close()
+        classes.append({
+            'name': line['Group'],
+            'type': course_type,
+            'course': code,
+            'users': [],
+            'duration': timeparse(line['Duration']) // 60,
+        })
+
+    # Unique courses only
+    courses = [dict(t) for t in {tuple(d.items()) for d in courses}]
+    print(courses)
+    print(classes)
+
+    # course_objects = set()
+    # class_objects = []
+
+    # file.readline()
+    # classes = [a.split("\t") for a in file.readlines()]
+    # for cl in classes:
+    #     course_objects.append({
+    #         'code': cl['code']
+    #     })
+
+    #     class_objects.append({
+    #         'code': cl[0],
+    #         'description': cl[1],
+    #         'group': cl[2],
+    #         'activity': cl[3],
+    #         'day': cl[4],
+    #         'time': cl[5],
+    #         # Output duration in minutes
+    #         'duration': timeparse(cl[10]) // 60
+    #     })
+
+    # print(class_objects)
