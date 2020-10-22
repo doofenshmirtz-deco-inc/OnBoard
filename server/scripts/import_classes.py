9412f223141de5fbe5dcce6ba9bebe6d4022333e
mattchrlw <mattchrlw@gmail.com>
2020-10-22 11:41:03 +1000

    Working script, but doesn't feed to GraphQL yet

--- server/scripts/import_classes.py
+++ server/scripts/import_classes.py
@@ -1,70 +1,99 @@
 from pytimeparse.timeparse import timeparse
+from collections import defaultdict
+import dateparser
+import datetime
 import csv
+import pprint
+import os
 
-# Deal with stupid double tabs in Allocate+ output
-with open("classes/Matthew_Low_matt.txt", "r+") as file:
-    content = file.read()
-    file.seek(0)
-    file.truncate()
-    file.write(content.replace('\t\t', '\t'))
+def parse_file(filename):
+    # Deal with stupid double tabs and newlines in Allocate+ output
+    with open(filename, "r+") as file:
+        content = file.read()
+        file.seek(0)
+        file.truncate()
+        file.write(content.replace('\t\t', '\t').replace('\n\n', ''))
 
-with open("classes/Matthew_Low_matt.txt", "r") as csvfile:
-    # Deal with extra tab
-    courses = []
-    classes = []
+    with open(filename, "r") as csvfile:
+        # Deal with extra tab
+        courses = []
+        classes = []
 
-    reader = csv.DictReader(csvfile, delimiter="\t")
-    for line in reader:
-        code, sem, _, _ = line['Subject Code'].split("_")
-        courses.append({
-            'code': code,
-            'name': line['Description'],
-            'semester': sem,
-            'level': "CourseLevel.Undergrad" if int(code[4]) < 7 else "CourseLevel.Postgrad",
-            'year': 2020
-        })
+        reader = csv.DictReader(csvfile, delimiter="\t")
+        for line in reader:
+            code, sem, _, _ = line['Subject Code'].split("_")
+            courses.append({
+                'code': code,
+                'name': line['Description'],
+                'semester': sem,
+                'level': "CourseLevel.Undergrad" if int(code[4]) < 7 else "CourseLevel.Postgrad",
+                'year': 2020
+            })
 
-        if "LEC" in line['Group']:
-            course_type = "ClassType.Lecture"
-        elif "TUT" in line['Group']:
-            course_type = "ClassType.Tutorial"
-        elif "PRA" in line['Group']:
-            course_type = "ClassType.Practical"
+            if "LEC" in line['Group']:
+                course_type = "ClassType.Lecture"
+            elif "TUT" in line['Group']:
+                course_type = "ClassType.Tutorial"
+            elif "PRA" in line['Group']:
+                course_type = "ClassType.Practical"
+            else:
+                course_type = "ClassType.Lecture"
+
+            dates = [dateparser.parse(j, settings={'DATE_ORDER': 'DMY'}) \
+                    + datetime.timedelta(minutes=timeparse(line['Time'])) for i in line['Dates'].split(", ") for j in i.split("-")]
+
+            date_starts, date_ends = [dates[0], dates[2]], [dates[1], dates[3]]
+            dates_full = []
+
+            for idx, i in enumerate(date_starts):
+                current_date = date_starts[idx]
+                dates_full.append(current_date)
+                while current_date < date_ends[idx]:
+                    current_date += datetime.timedelta(days=7)
+                    dates_full.append(current_date) 
+
+            # print(dateparser.parse(line['Dates']) + datetime.timedelta(minutes=timeparse(line['Time'])))
+
+            classes.append({
+                'name': line['Group'] + "-" + line['Activity'],
+                'type': course_type,
+                'course': code,
+                'users': [filename.split("_")[2].split(".txt")[0]],
+                'duration': timeparse(line['Duration']) // 60,
+                'dates': dates_full
+            })
+
+        # Unique courses only
+        courses = [dict(t) for t in {tuple(d.items()) for d in courses}]
+        return courses, classes
+
+if __name__ == "__main__":
+    files = os.listdir("classes")
+    if '.DS_Store' in files:
+        files.remove('.DS_Store')
+    all_courses = []
+    all_classes = []
+    for file in files:
+        print("-----------------", file, "-----------------")
+        courses, classes = parse_file(os.path.join("classes", file))
+        all_courses += courses
+        all_classes += classes
+
+    all_courses = sorted([dict(t) for t in {tuple(d.items()) for d in all_courses}], key=lambda i: i['code'])
+    all_classes = sorted(all_classes, key=lambda i: i['course'])
+
+    pprint.pprint(all_classes)
+
+    merged_classes = []
+
+    # Construct user list for each course
+    for dictionary in all_classes:
+        print(dictionary)
+        if len(list(filter(lambda a: a['name'] == dictionary['name'] and a['course'] == dictionary['course'], merged_classes))) == 0:
+            merged_classes.append(dictionary)
+            print("New class", dictionary)
         else:
-            course_type = "ClassType.Lecture"
+            next(filter(lambda a: a['name'] == dictionary['name'] and a['course'] == dictionary['course'], merged_classes))["users"].extend(dictionary["users"])
 
-        classes.append({
-            'name': line['Group'],
-            'type': course_type,
-            'course': code,
-            'users': [],
-            'duration': timeparse(line['Duration']) // 60,
-        })
-
-    # Unique courses only
-    courses = [dict(t) for t in {tuple(d.items()) for d in courses}]
-    print(courses)
-    print(classes)
-
-    # course_objects = set()
-    # class_objects = []
-
-    # file.readline()
-    # classes = [a.split("\t") for a in file.readlines()]
-    # for cl in classes:
-    #     course_objects.append({
-    #         'code': cl['code']
-    #     })
-
-    #     class_objects.append({
-    #         'code': cl[0],
-    #         'description': cl[1],
-    #         'group': cl[2],
-    #         'activity': cl[3],
-    #         'day': cl[4],
-    #         'time': cl[5],
-    #         # Output duration in minutes
-    #         'duration': timeparse(cl[10]) // 60
-    #     })
-
-    # print(class_objects)
+    pprint.pprint(all_courses)
+    pprint.pprint(merged_classes)
