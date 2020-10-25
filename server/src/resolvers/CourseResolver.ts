eb8ffd3acda64c331961dde3f9c0aa41d03cec32
Tom Cranitch <tom@cranitch.com.au>
2020-10-25 22:19:58 +1000

    WIP

--- server/src/resolvers/CourseResolver.ts
+++ server/src/resolvers/CourseResolver.ts
@@ -7,9 +7,13 @@ import {
   UseMiddleware,
   Authorized,
   Ctx,
+  FieldResolver,
+  Root,
 } from "type-graphql";
 import { PaginationArgs, getOrder } from "./Types";
 import { Course } from "../models/Course";
+import { User } from "../models/User";
+import { CourseGroupPair } from "../models/CourseGroupPair";
 
 @Resolver((of) => Course)
 export class CourseResolver {
@@ -29,5 +33,19 @@ export class CourseResolver {
   @Authorized()
   async course(@Arg("courseID", () => ID) courseID: number) {
     return await Course.findOne({ id: courseID });
+  }
+
+  @FieldResolver(() => [User])
+  async staff(@Root() course: Course) {
+    return [];
+    console.log("here");
+
+    let query = User.createQueryBuilder("user")
+      .leftJoinAndSelect("user.group", "group")
+      .leftJoinAndSelect("group.coursePair", "cgp")
+      .leftJoinAndSelect("cgp.course", "course")
+      .where("course.id = :id", { id: course.id });
+
+    console.log(await query.getMany());
   }
 }
